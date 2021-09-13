ed15308eed5f4c7dc23f5890781f4393ed6460a9
vedang <vedangr20@gmail.com>
2021-09-14 00:16:20 +0530

    Initialized the Repository

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public DefaultTrackableEventHandler[] dteh;
+    public GameObject[] players;
+    public GameObject[] playerhands;
+    public GameObject flames;
+
+    public int time = 5;
+
+    void Update()
+    {
+        GameObject player1 = null;
+        GameObject player2 = null;
+
+
+        GameObject hand1 = null;
+        GameObject hand2 = null;
+
+        for (int i = 0; i < dteh.Length; i++)
+        {
+            if (dteh[i].Tracked == true && players[i].tag != "dead")
+            {
+                if (player1 == null)
+                {
+                    player1 = players[i];
+                    hand1 = playerhands[i];
+                }
+                else if (player2 == null)
+                {
+                    player2 = players[i];
+                    hand2 = playerhands[i];
+                }
+            }
+        }
+        if(player2 != null)
+        {
+            player1.transform.LookAt(player2.transform,player1.transform.up);
+            player2.transform.LookAt(player1.transform, player2.transform.up);
+            Attributes p1 = player1.GetComponent<Attributes>();
+            Attributes p2 = player2.GetComponent<Attributes>();
+            if(((p1.health+p1.mana)/200.0f > (p2.health+p2.mana)/200.0f) && p1.attacked == false)
+            {
+                p1.attacked = true;
+                player1.GetComponent<Animator>().SetTrigger("Attack");
+                p1.gameObject.GetComponent<Collider>().enabled = false;
+                hand1.SetActive(true);
+                StartCoroutine(waitforsecods(hand1,p1,player2));
+            }
+            else if(((p1.health + p1.mana) / 200.0f < (p2.health + p2.mana) / 200.0f) && p2.attacked == false)
+            {
+                p2.attacked = true;
+                player2.GetComponent<Animator>().SetTrigger("Attack");
+                p2.gameObject.GetComponent<Collider>().enabled = false;
+                hand2.SetActive(true);
+                StartCoroutine(waitforsecods(hand2,p2,player1));
+            }
+        }
+    }
+
+    IEnumerator waitforsecods(GameObject hand, Attributes p, GameObject player)
+    {
+        yield return new WaitForSeconds(time);
+        hand.SetActive(false);
+        GameObject flame = Instantiate(flames, hand.transform.position, hand.transform.rotation);
+        flame.GetComponent<Flame>().target = player;
+        flame.GetComponent<Flame>().player = p;
+        flame.GetComponent<Flame>().play();
+
+    }
+}
---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour
+{
+    public Attributes player;
+
+    public GameObject target;
+    public GameObject burst;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Animator>().SetBool("Dead", true);
+        other.tag = "dead";
+        player.attacked = false;
+        player.gameObject.GetComponent<Collider>().enabled = true;
+        GameObject a = Instantiate(burst, transform.position, transform.rotation);
+        Destroy(a, 2);
+        Destroy(gameObject);
+    }
+
+    public void play()
+    {
+        StartCoroutine(chase());
+    }
+
+    public IEnumerator chase() {
+        while(true)
+        {
+            yield return new WaitForSeconds(0.01f);
+            if (transform.position == target.transform.position)
+                break;
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * 5);
+            }
+        }
+    }
+}
